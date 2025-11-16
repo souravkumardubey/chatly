47c7d7cc1b3bfad7c480452e72d49f4f52c8f2ce
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-17 00:07:17 +0530

    feat: establish MongoDB connection for backend service

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config();
+connectDB();
 const app = express();
 const port = process.env.PORT;
 app.listen(port, () => {
