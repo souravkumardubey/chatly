7cc44db4fc4f65c32e411a1fb7bda84d141c5703
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-17 20:20:51 +0530

    config: setup redis connection

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import { createClient } from "redis";
 dotenv.config();
 connectDB();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false,
+    },
+});
+redisClient.connect().then(() => {
+    console.log('Connected to Redis');
+}).catch((err) => {
+    console.error('Redis connection error:', err);
+});
 const app = express();
 const port = process.env.PORT;
 app.listen(port, () => {
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import {createClient} from "redis"
 
 dotenv.config();
 connectDB();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
+
+redisClient.connect().then(() => {
+  console.log('Connected to Redis');
+}).catch((err) => {
+  console.error('Redis connection error:', err);
+});
 
 const app = express();
 
