cca19aa37579fa216cf21111402c1b3d25425b37
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-20 23:51:36 +0530

    feat: rabbitmq connection

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 dotenv.config();
 connectDB();
+connectRabbitMQ();
 export const redisClient = createClient({
     url: process.env.REDIS_URL,
     socket: {
@@ -11,12 +14,16 @@ export const redisClient = createClient({
         rejectUnauthorized: false,
     },
 });
-redisClient.connect().then(() => {
-    console.log('Connected to Redis');
-}).catch((err) => {
-    console.error('Redis connection error:', err);
+redisClient
+    .connect()
+    .then(() => {
+    console.log("Connected to Redis");
+})
+    .catch((err) => {
+    console.error("Redis connection error:", err);
 });
 const app = express();
+app.use("api/v1", userRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`User service is running at http://localhost:${port}`);
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { createClient } from "redis";
 import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 
 dotenv.config();
 connectDB();
+connectRabbitMQ();
 
 export const redisClient = createClient({
   url: process.env.REDIS_URL,
