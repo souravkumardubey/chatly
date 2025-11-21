d05b1ca1b18de582d8c097b5d42256e2762d10c4
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-21 23:15:28 +0530

    feat: add user login route

--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { loginUser } from "../controllers/user.js";
 
 const userRoutes = express.Router();
+
+userRoutes.post("/login", loginUser)
 
 export default userRoutes;
