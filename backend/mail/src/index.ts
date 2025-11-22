ee1da9a73123f940b753d39b9d934ec4ff8afab3
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-22 11:17:12 +0530

    feat: initialize mail service

---
+++ backend/mail/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { sendOtpConsumer } from './consumer.js';
+
+dotenv.config();
+sendOtpConsumer();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Mail service is running on port ${PORT}`);
+});
