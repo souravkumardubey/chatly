093ad91a779e4d6b8ee5518f5341373440459ff4
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-23 14:52:30 +0530

    route: setup verifyUser route

--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { loginUser } from "../controllers/user.js";
+import { loginUser, verifyUser } from "../controllers/user.js";
 
 const userRoutes = express.Router();
 
-userRoutes.post("/login", loginUser)
+userRoutes.post("/login", loginUser);
+userRoutes.post("/verify", verifyUser);
 
 export default userRoutes;
