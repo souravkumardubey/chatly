b9622deafbaaa25a7cb9e6a8ee8c3f675e2e2b49
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-11-26 20:59:10 +0530

    init: cors

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import { createClient } from "redis";
 import userRoutes from "./routes/user.js";
 import { connectRabbitMQ } from "./config/rabbitmq.js";
+import cors from "cors";
 dotenv.config();
 connectDB();
 connectRabbitMQ();
@@ -24,6 +25,7 @@ redisClient
 });
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1", userRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
