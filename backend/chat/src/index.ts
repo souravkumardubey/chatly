c27601cd0dcc0796242a726ff78d18d6872ba0cc
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-12-08 00:09:50 +0530

    feat: setup db connection

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import chatRoutes from "./routes/chat.js";
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/api/v1", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Chat Server is running on port ${PORT}`);
