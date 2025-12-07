048c7f5d8408df62e2ab552809eaba2851dca847
Sourav Kumar DUBEY <skdlp2000@gmail.com>
2025-12-08 00:11:40 +0530

    feat: setup isAuth middleware for routes

---
+++ backend/chat/src/middlewares/isAuth.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface IUser extends Document {
+    _id: string,
+    name: string,
+    email: string,
+}
+
+export interface AuthenticatedRequest extends Request {
+    user?: IUser | null;
+}
+
+export const isAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            res.status(401).json({ message: 'Please login - No Auth Headers' });
+            return;
+        }
+        const token = authHeader.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+        if (!decodedToken || !decodedToken.user) {
+            res.status(401).json({ message: "Invalid Token" });
+            return;
+        }
+        req.user = decodedToken.user;
+        next();
+    } catch (error: any) {
+        console.error("JWT Verification Error:", error);
+        res.status(401).json({ message: "Please login - Jwt Error", error: error.message });
+        return;
+    }
+}
+
+export default isAuth;
